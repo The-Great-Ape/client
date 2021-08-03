fe1c8a4102c1f512ee0e009504fc23732e55c5dc
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2021-08-03 19:32:28 +0100

    :fire: remove imports :art: format code

--- src/lib/wallet/Wallet.ts
+++ src/lib/wallet/Wallet.ts
@@ -1,198 +1,192 @@
 // @ts-ignore
-import SolanaWalletAdapter from '@project-serum/sol-wallet-adapter';
-import { useSession } from "../../contexts/session";
+import SolanaWalletAdapter from "@project-serum/sol-wallet-adapter";
 
 import {
-    AccountInfo,
-    Connection,
-    PublicKey,
-    clusterApiUrl,
-    Cluster,
-    Transaction,
-    SystemProgram,
-    RpcResponseAndContext,
-} from '@solana/web3.js'
-import Session from '../../models/Session';
-
+  Connection,
+  PublicKey,
+  clusterApiUrl,
+  Cluster,
+  Transaction,
+  SystemProgram,
+} from "@solana/web3.js";
+import Session from "../../models/Session";
 
 const PROVIDERS: { [key: string]: string } = {
-    SOLLET: "https://www.sollet.io",
+  SOLLET: "https://www.sollet.io",
 };
 
 const NETWORKS: { [key: string]: Cluster } = {
-    MAINNET: "mainnet-beta",
-    DEVNET: "devnet"
+  MAINNET: "mainnet-beta",
+  DEVNET: "devnet",
 };
 
 class Wallet {
-    //Properties
-    wallet: typeof SolanaWalletAdapter;
-    connection: Connection;
-    publicKey: PublicKey;
-    isConnected: boolean;
-    discordInfo: Object;
-    session: Object;
-    onChange: (wallet: Wallet | null) => void;
+  //Properties
+  wallet: typeof SolanaWalletAdapter;
+  connection: Connection;
+  publicKey: PublicKey;
+  isConnected: boolean;
+  discordInfo: Object;
+  session: Object;
+  onChange: (wallet: Wallet | null) => void;
 
-    //Constructor
-    constructor(provider: string = "SOLLET", network: string = "MAINNET") {
-        let providerUrl = PROVIDERS[provider];
-        this.connection = new Connection(clusterApiUrl(NETWORKS["MAINNET"]));
+  //Constructor
+  constructor(provider: string = "SOLLET", network: string = "MAINNET") {
+    let providerUrl = PROVIDERS[provider];
+    this.connection = new Connection(clusterApiUrl(NETWORKS["MAINNET"]));
 
-        // switch(provider){
+    // switch(provider){
 
-        // }
+    // }
 
-        if ((window as any).sollet){
-            this.wallet = new SolanaWalletAdapter((window as any).sollet);
+    if ((window as any).sollet) {
+      this.wallet = new SolanaWalletAdapter((window as any).sollet);
+    } else {
+      this.wallet = new SolanaWalletAdapter(providerUrl);
+    }
 
-        }else{
-            this.wallet = new SolanaWalletAdapter(providerUrl);
+    this.discordInfo = null;
+    this.onChange = () => {};
+    this.wallet.on("connect", this.onConnect.bind(this));
+    this.wallet.on("disconnect", this.onDisconnect.bind(this));
+  }
+
+  //Events
+  async onConnect(publicKey: PublicKey) {
+    this.publicKey = publicKey;
+    this.isConnected = true;
+    this.onChange(this);
+  }
+
+  async onDisconnect() {
+    this.isConnected = false;
+    this.onChange(null);
+  }
+
+  //Functions
+  async connect() {
+    try {
+      await this.wallet.connect();
+    } catch (e) {
+      console.warn(e);
+    }
+  }
+
+  async disconnect() {
+    try {
+      await this.wallet.disconnect();
+    } catch (e) {
+      console.warn(e);
+    }
+  }
+
+  async sendTransaction() {
+    try {
+      let transaction = new Transaction().add(
+        SystemProgram.transfer({
+          fromPubkey: this.publicKey,
+          toPubkey: this.publicKey,
+          lamports: 100,
+        })
+      );
+      transaction.recentBlockhash = (
+        await this.connection.getRecentBlockhash()
+      ).blockhash;
+
+      transaction.feePayer = this.publicKey;
+      let signed = await this.wallet.signTransaction(transaction);
+      let signature = await this.connection.sendRawTransaction(
+        signed.serialize()
+      );
+      await this.connection.confirmTransaction(signature, "singleGossip");
+    } catch (e) {
+      console.warn(e);
+    }
+  }
+
+  toHex(buffer: Uint8Array | string) {
+    return Array.prototype.map
+      .call(buffer, (x) => ("00" + x.toString(16)).slice(-2))
+      .join("");
+  }
+
+  async signMessage(token: string) {
+    if (!(window as any).sollet) {
+      this.wallet._popup.focus();
+    } else {
+      console.log(this.wallet.window);
+      window.focus();
+    }
+
+    try {
+      const data = new TextEncoder().encode("$GRAPE");
+      const signed = await this.wallet.sign(data, "utf8");
+      //this.wallet.popup.focus();
+      const signature = signed.signature;
+      const publicKey = this.publicKey.toBase58();
+      const address = this.publicKey.toBuffer();
+
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          token: token,
+          address,
+          publicKey,
+          signature,
+        }),
+      });
+
+      const session = await response.json();
+      session.token = { address, signature };
+      session.publicKey = publicKey;
+
+      return new Session(session);
+    } catch (e) {
+      console.warn(e);
+    }
+  }
+
+  async register(token: string, userId: string) {
+    try {
+      //if (!(window as any).sollet){
+      this.wallet._popup.focus();
+      //}
+
+      const data = new TextEncoder().encode("$GRAPE");
+      const signed = await this.wallet.sign(data, "utf8");
+      const signature = signed.signature;
+      const publicKey = this.publicKey.toBase58();
+      const address = this.publicKey.toBuffer();
+
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}/register`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            userId,
+            token: token,
+            address,
+            publicKey,
+            signature,
+          }),
         }
+      );
 
-        this.discordInfo = null;
-        this.onChange = () => { };
-        this.wallet.on('connect', this.onConnect.bind(this));
-        this.wallet.on('disconnect', this.onDisconnect.bind(this));
+      const session = await response.json();
+      session.token = { address, signature };
+      session.publicKey = publicKey;
+
+      return new Session(session);
+    } catch (e) {
+      console.warn(e);
     }
-
-    //Events
-    async onConnect(publicKey: PublicKey) {
-        this.publicKey = publicKey;
-        this.isConnected = true;
-        this.onChange(this);
-    }
-
-
-    async onDisconnect() {
-        this.isConnected = false;
-        this.onChange(null);
-    }
-
-
-    //Functions
-    async connect() {
-        try {
-            await this.wallet.connect();
-        } catch (e) {
-            console.warn(e);
-        }
-    }
-
-    async disconnect() {
-        try {
-            await this.wallet.disconnect();
-        } catch (e) {
-            console.warn(e);
-        }
-    }
-
-    async sendTransaction() {
-        try {
-            let transaction = new Transaction().add(
-                SystemProgram.transfer({
-                    fromPubkey: this.publicKey,
-                    toPubkey: this.publicKey,
-                    lamports: 100,
-                })
-            );
-            transaction.recentBlockhash = (
-                await this.connection.getRecentBlockhash()
-            ).blockhash;
-
-            transaction.feePayer = this.publicKey;
-            let signed = await this.wallet.signTransaction(transaction);
-            let signature = await this.connection.sendRawTransaction(signed.serialize());
-            await this.connection.confirmTransaction(signature, 'singleGossip');
-        } catch (e) {
-            console.warn(e);
-        }
-    }
-
-    toHex(buffer: Uint8Array | string) {
-        return Array.prototype.map
-            .call(buffer, (x) => ('00' + x.toString(16)).slice(-2))
-            .join('');
-    }
-
-    async signMessage(token: string) {
-        if (!(window as any).sollet){
-            this.wallet._popup.focus();
-
-        }else{
-            console.log(this.wallet.window);
-            window.focus()
-
-        }
-
-        try {
-
-    
-            const data = new TextEncoder().encode('$GRAPE');
-            const signed = await this.wallet.sign(data, 'utf8');
-            //this.wallet.popup.focus();
-            const signature = signed.signature;
-            const publicKey = this.publicKey.toBase58();
-            const address = this.publicKey.toBuffer();
-
-            const response = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    token: token,
-                    address,
-                    publicKey,
-                    signature
-                })
-            });
-
-            const session = await response.json();
-            session.token = {address, signature};
-            session.publicKey = publicKey;
-
-            return new Session(session);
-        } catch (e) {
-            console.warn(e);
-        }
-    }
-
-    async register(token: string, userId: string) {
-        try {
-            //if (!(window as any).sollet){
-                this.wallet._popup.focus();
-            //}
-
-            const data = new TextEncoder().encode('$GRAPE');
-            const signed = await this.wallet.sign(data, 'utf8');
-            const signature = signed.signature;
-            const publicKey = this.publicKey.toBase58();
-            const address = this.publicKey.toBuffer();
-
-            const response = await fetch(`${process.env.REACT_APP_API_URL}/register`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    userId,
-                    token: token,
-                    address,
-                    publicKey,
-                    signature
-                })
-            });
-
-            const session = await response.json();
-            session.token = {address, signature};
-            session.publicKey = publicKey;
-            
-            return new Session(session);
-        } catch (e) {
-            console.warn(e);
-        }
-    }
+  }
 }
 
 export default Wallet;
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,32 +1,31 @@
 import React from "react";
-import Container from '@material-ui/core/Container';
+import Container from "@material-ui/core/Container";
 import { useSession } from "../../contexts/session";
-import Box from '../../components/Box/Box';
-import './Home.less';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { ServersView, SettingsView, ConfirmationView, RegisterView, PortfolioView } from "../";
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import Box from "../../components/Box/Box";
+import "./Home.less";
+import { ServersView, SettingsView, PortfolioView } from "../";
+import Grid from "@material-ui/core/Grid";
 
-export const HomeView = (props) => {
-
-  const { session, setSession } = useSession();
+export const HomeView = () => {
+  const { session } = useSession();
   const isConnected = session && session.isConnected;
-  const publicKey = session && session.publicKey;
   return (
-    <div className="home main" >
-      {isConnected ? <Grid container spacing={3}>
-        <Grid item lg={6} md={12} sm={12}>
-          <PortfolioView/>
+    <div className="home main">
+      {isConnected ? (
+        <Grid container spacing={3}>
+          <Grid item lg={6} md={12} sm={12}>
+            <PortfolioView />
+          </Grid>
+          <Grid item lg={6} md={12} sm={12}>
+            <ServersView />
+            <SettingsView />
+          </Grid>
         </Grid>
-        <Grid item lg={6} md={12} sm={12}>
-          <ServersView />
-          <SettingsView />
-        </Grid>
-      </Grid> : <Container maxWidth="md" className="main">
-        <Box>{'Not connected'}</Box>
-      </Container>}
+      ) : (
+        <Container maxWidth="md" className="main">
+          <Box>{"Not connected"}</Box>
+        </Container>
+      )}
     </div>
   );
-}
+};
